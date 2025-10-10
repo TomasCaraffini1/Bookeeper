ea1c5e63e089b8d556103befaaa3236833fe9f3b
Guido-c66 <gbotta@uade.edu.ar>
2025-10-10 18:54:25 -0300

    GB | Agrego archivo lista.py

---
+++ libros/lista.py
@@ -0,0 +1,31 @@
+#    ¡¡¡ FUNCION OK !!!
+def listar_libros(biblioteca):
+    # Muestra por pantalla la lista de libros
+
+    if not biblioteca:
+        print("\nNo hay libros cargados.")
+        return
+
+    encabezado = ["Título", "Autor", "Género", "Año", "Estado"]
+
+    # Anchos dinámicos
+    titulo_w = max(len(encabezado[0]), *(len(f["titulo"]) for f in biblioteca))
+    autor_w  = max(len(encabezado[1]), *(len(f["autor"])  for f in biblioteca))
+    genero_w = max(len(encabezado[2]), *(len(f["genero"]) for f in biblioteca))
+    anio_w = 4
+    estado_w = 10
+
+    # Calcula largo total de la tabla e imprime con una funcion lambda
+    largo = 21 + titulo_w + autor_w + genero_w + anio_w + estado_w
+    separador = lambda x: print("-" * x)
+    
+    print()
+    # Encabezado
+    separador(largo)
+    print(f"| {encabezado[0].ljust(titulo_w)}  | {encabezado[1].ljust(autor_w)}  | {encabezado[2].ljust(genero_w)}  | {encabezado[3].ljust(anio_w)}  | {encabezado[4].ljust(estado_w)}  |")
+
+    # Filas
+    separador(largo)
+    for f in biblioteca:
+        print(f"| {f["titulo"].ljust(titulo_w)}  | {f["autor"].ljust(autor_w)}  | {f["genero"].ljust(genero_w)}  | {str(f["año"]).ljust(anio_w)}  | {f["estado"].ljust(estado_w)}  |")
+    separador(largo)
